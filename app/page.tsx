7ee8e8f62525f6c63c2dd5837c2886007bfc6825
soumiya59 <soumiyaus@gmail.com>
2023-02-05 19:34:23 +0100

    added getclothing fn

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import moment from "moment";
-import ShowData from './ShowData';
+
 export default function App() {
   const [lat, setLat] = useState({});
   const [long, setLong] = useState({});
@@ -23,16 +23,26 @@ export default function App() {
     }
     fetchData();
   }, [lat,long])
-console.log(data)
 
-return (
+  const getClothingMsg = () => {
+    if (data.main.temp_min <5 && data.main.temp_max>10) {
+      return <span>a thick jumper</span>;
+    } else if (data.temp_min>20 && data.main.temp_max>20) {
+      return <span>shorts</span>;
+    } else if (data.temp_min <20 && data.main.temp_max>20) {
+      return <span>shorts and a jumper</span>;
+    } else {
+      return <span>something not too thin and not too thick</span>;
+    }
+  };
+  return (
     <div className="container mx-auto">
       <div className="mt-3 mx-auto flex justify-center font-bold text-3xl" style={{ width: '60vw' }}>
         {data.main ? (
-          <div className="card-body text-center">
-            <Image
+          <div className="text-center">
+              <Image
               src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} alt="weather status icon" width={300} height={300} />
-            <div className="row mt-4">
+              <div className="">
                  City Name: {data.name}
                 <p>Temprature: {data.main.temp} &deg;C</p>
                 <p>Sunrise: {new Date(data.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
@@ -41,13 +51,14 @@ return (
                 <p>Humidity: {data.main.humidity} %</p>
                 <p>Day: {moment().format('dddd')}</p>
                 <p>Date: {moment().format('LL')}</p>
-            </div>
+                <p> wear {getClothingMsg()} </p>
+              </div>
           </div>
         ) : (
           <h1>Loading</h1>
         )}
       </div>
-  </div>
-);
+    </div>
+  );
 
 }
